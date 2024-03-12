a656c417b88d2d6bcac489c789cd003ab705f483
frnbrz <frnbrz@gmail.com>
2024-03-12 20:55:32 +0100

    Add language selector to CodeEditor component

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,6 +1,9 @@
 import { Box } from "@chakra-ui/react"
 import Editor from "@monaco-editor/react"
 import { useRef, useState } from "react"
+import { CODE_SNIPPETS } from "../data/languages.data"
+import LanguageSelector from "./LanguageSelector"
+
 
 
 interface IEditor {
@@ -10,6 +13,7 @@ interface IEditor {
 function CodeEditor() {
   const editorRef = useRef<IEditor | null>(null)
   const [value, setValue] = useState('')
+  const [language, setLanguage] = useState('javascript')
   const handleValueChange = (newValue: string | undefined) => {
     setValue(newValue || '')
     console.log(newValue)
@@ -20,13 +24,21 @@ function CodeEditor() {
     editor.focus()
   }
 
+  const handleSelect = (language: string) => {
+    setLanguage(language)
+    setValue(
+      CODE_SNIPPETS[language as keyof typeof CODE_SNIPPETS]
+    )
+  }
+
   return (
     <Box>
+      <LanguageSelector onSelect={handleSelect} language={language} />
       <Editor
         height="75vh"
         theme="vs-dark"
-        defaultLanguage="javascript"
-        defaultValue=""
+        defaultLanguage={language}
+        defaultValue={CODE_SNIPPETS[language as keyof typeof CODE_SNIPPETS]}
         onMount={handleEditorDidMount}
         value={value}
         onChange={handleValueChange}
