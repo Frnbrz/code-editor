d662365f56be68c749518183446250ac4d5c0d29
Mona Lisa <johndoe@example.com>
2024-03-13 13:34:56 +0100

    Refactor CodeEditor component

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,8 +1,8 @@
-import { Box } from "@chakra-ui/react"
-import Editor from "@monaco-editor/react"
+import { Box, HStack } from "@chakra-ui/react"
 import { useRef, useState } from "react"
-import { CODE_SNIPPETS } from "../data/languages.data"
-import LanguageSelector from "./LanguageSelector"
+import EditorComponent from "./EditorComponent"
+import Output from "./Output"
+
 
 
 
@@ -10,39 +10,17 @@ interface IEditor {
   focus: () => void
 }
 
+
+
 function CodeEditor() {
-  const editorRef = useRef<IEditor | null>(null)
-  const [value, setValue] = useState('')
   const [language, setLanguage] = useState('javascript')
-  const handleValueChange = (newValue: string | undefined) => {
-    setValue(newValue || '')
-    console.log(newValue)
-  }
-
-  const handleEditorDidMount = (editor: IEditor) => {
-    editorRef.current = editor
-    editor.focus()
-  }
-
-  const handleSelect = (language: string) => {
-    setLanguage(language)
-    setValue(
-      CODE_SNIPPETS[language as keyof typeof CODE_SNIPPETS]
-    )
-  }
-
+  const editorRef = useRef<IEditor | null>(null)
   return (
     <Box>
-      <LanguageSelector onSelect={handleSelect} language={language} />
-      <Editor
-        height="75vh"
-        theme="vs-dark"
-        defaultLanguage={language}
-        defaultValue={CODE_SNIPPETS[language as keyof typeof CODE_SNIPPETS]}
-        onMount={handleEditorDidMount}
-        value={value}
-        onChange={handleValueChange}
-      />
+      <HStack spacing={4}>
+        <EditorComponent editorRef={editorRef} language={language} setLanguage={setLanguage} />
+        <Output editorRef={editorRef} language={language} setLanguage={setLanguage} />
+      </HStack>
     </Box>
   )
 }
