217f938570890dd2e605634a41f9ff4d2504e05f
frnbrz <frnbrz@gmail.com>
2024-03-22 21:11:58 +0100

    Refactor CodeEditor component

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -12,14 +12,27 @@ interface IEditor {
 
 
 
+interface IEditor {
+  focus: () => void
+  getValue: () => string | undefined
+}
+
 function CodeEditor() {
   const [language, setLanguage] = useState('javascript')
   const editorRef = useRef<IEditor | null>(null)
   return (
     <Box>
       <HStack spacing={4}>
-        <EditorComponent editorRef={editorRef} language={language} setLanguage={setLanguage} />
-        <Output editorRef={editorRef} language={language} setLanguage={setLanguage} />
+        <EditorComponent
+          editorRef={editorRef}
+          language={language}
+          setLanguage={setLanguage}
+        />
+        <Output
+          editorRef={editorRef}
+          language={language}
+          setLanguage={setLanguage}
+        />
       </HStack>
     </Box>
   )
