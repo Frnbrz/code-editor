6ac810b138e0beab1cde42828fc78701f193b77e
frnbrz <frnbrz@gmail.com>
2024-05-14 23:36:25 +0200

    Update language versions in languages.data.ts

--- src/data/languages.data.ts
+++ src/data/languages.data.ts
@@ -1,11 +1,10 @@
 export const LANGUAGES = {
-
-  javascript: "18.15.0",
-  typescript: "5.3.0",
-  python: "3.9.7",
-  java: "16.0.2",
-  go: "1.17",
-  rust: "1.55.0"
+  javascript: '18.15.0',
+  typescript: '5.0.3',
+  python: '3.10.0',
+  java: '15.0.2',
+  go: '1.16.2',
+  rust: '1.68.2',
 }
 
 export const CODE_SNIPPETS = {
@@ -14,5 +13,5 @@ export const CODE_SNIPPETS = {
   python: `\ndef greet(name):\n\tprint("¡Hola, " + name + "!")\n\ngreet("Fran")\n`,
   java: `\npublic class Main {\n\tpublic static void main(String[] args) {\n\t\tSystem.out.println("¡Hola, Mundo!");\n\t}\n}\n`,
   go: `\npackage main\n\nimport "fmt"\n\nfunc main() {\n\tfmt.Println("¡Hola, Mundo!")\n}\n`,
-  rust: `\nfn main() {\n\tprintln!("¡Hola, Mundo!");\n}\n`
+  rust: `\nfn main() {\n\tprintln!("¡Hola, Mundo!");\n}\n`,
 }
